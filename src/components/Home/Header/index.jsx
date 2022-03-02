88dac50f47ec2be3a4249f126f5f3d2db6e4ed7c
user name <bkcsplayer@gmail.com>
2022-03-01 23:40:09 -0700

    first commit

---
+++ src/components/Home/Header/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './styles.css';
+
+const Header = () => (
+  <header className='home-header'>
+    <h2>News</h2>
+    <h1>
+      <span>“</span> Khtain Digital <span>”</span>
+    </h1>
+    <p>
+      每日更新 <br /> 
+    </p>
+  </header>
+);
+
+export default Header;
